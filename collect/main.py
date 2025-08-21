3ab6a6c4aa82090b755fab301f3810e25372146e
Patrick Hayes <hayespatrick46@gmail.com>
2025-08-21 01:00:11 -0700

    setup

---
+++ collect/main.py
@@ -0,0 +1,13 @@
+import serial
+import pynicom
+
+def find_ports():
+
+    pass
+
+#pynicom -port= -baud=115200
+''' maybe format as class
+1. need to let user see all and select a port to use
+
+honestly could do serial port stuff last can just hard code port num for now to test
+'''
