1e6d26f2d87b64c2d9e224764e1c6d7fe12f0ee4
Patrick Hayes <hayespatrick46@gmail.com>
2026-06-28 16:00:40 -0700

    added data sources, but may need to further test serialsource in future

---
+++ foggui/sources.py
@@ -0,0 +1,36 @@
+import serial, time
+from serial.tools.list_ports import comports
+
+BAUDRATE = 115200
+
+class ReplaySource:
+    def __init__(self, path: str):
+        self.path = path
+
+    def lines(self):
+        with open(self.path) as f:
+            for line in f:
+                yield line
+
+class SerialSource:
+    def lines(self):
+        while True:
+            ser = None
+            for port in comports():
+                try:
+                    ser = serial.Serial(port.device, BAUDRATE, timeout=1)
+                    break
+                except serial.SerialException:
+                    continue
+            
+            if ser is None:
+                time.sleep(5)
+                continue
+                
+            with ser:
+                while True:
+                    try:
+                        line = ser.readline().decode("utf-8")
+                        yield line
+                    except serial.SerialException:
+                        break
