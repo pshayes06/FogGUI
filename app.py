28ac42e78ee4c6cfce19b2e7a096dbdc71a05ef1
Patrick Hayes <hayespatrick46@gmail.com>
2026-06-28 23:29:27 -0700

    building api

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import json
-from flask import Flask, Response, send_from_directory
+from flask import Flask, Response, send_from_directory, jsonify
 from foggui.sources import ReplaySource, SerialSource
 from foggui.parser import parse_packet
-from foggui.db import start_flight, insert_reading, end_flight
+from foggui.db import start_flight, insert_reading, end_flight, get_flights, get_readings
 
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ def home():
 
 @app.route("/stream")
 def stream():
-    source = ReplaySource("mockdata.txt", realtime=True) # temp hardcode
+    source = ReplaySource("nothing.txt", realtime=True) # temp hardcode
     flight_id = start_flight()
     def eventStream():
         try:
@@ -35,10 +35,19 @@ def stream():
                 
                 yield f"data: {data}\n\n"
             yield "data: done\n\n"
+            end_flight(flight_id)
         except GeneratorExit:
             end_flight(flight_id)
 
     return Response(eventStream(), mimetype="text/event-stream")
+
+@app.route("/api/flights")
+def api_get_flights():
+    return jsonify(get_flights())
+
+@app.route("/api/flights/<int:flight_id>/readings")
+def api_get_readings(flight_id):
+    return jsonify(get_readings(flight_id))
 
 if __name__ == '__main__':
     app.run(port=5001)
--- foggui/db.py
+++ foggui/db.py
@@ -21,5 +21,42 @@ def insert_reading(flight_id: int, reading: Reading) -> None:
 
 def end_flight(flight_id: int) -> None:
     with conn.cursor() as cur:
-        cur.execute("UPDATE flights SET ended_at = NOW() WHERE id = %s", flight_id)
+        cur.execute("UPDATE flights SET ended_at = NOW() WHERE id = %s", (flight_id,))
     conn.commit()
+
+def get_flights() -> list:
+    with conn.cursor() as cur:
+        cur.execute("SELECT id, started_at, ended_at, label FROM flights")
+        flights = cur.fetchall()
+    
+    result = []
+    for row in flights:
+        result.append({
+            "id": row[0],
+            "started_at": row[1],
+            "ended_at": row[2],
+            "label": row[3]
+        })
+    
+    return result
+
+def get_readings(flight_id: int) -> list:
+    with conn.cursor() as cur:
+        cur.execute("SELECT id, recorded_at, uptime_s, altitude_m, pressure_mb, temperature_c, humidity_pct FROM readings " \
+                    "WHERE flight_id = %s", (flight_id,))
+        readings = cur.fetchall()
+    
+    result = []
+    for row in readings:
+        result.append({
+            "id": row[0],
+            "recorded_at": row[1],
+            "uptime_s": row[2],
+            "altitude_m": row[3],
+            "pressure_mb": row[4],
+            "temperature_c": row[5],
+            "humidity_pct": row[6],
+        })
+    
+    return result
+        
